6851870bfdcad1bfba379c5306db91674135f5c0
Nielven Mascariñas <mascarinas022@gmail.com>
2024-06-14 09:42:13 +0800

    add login implementation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,29 +1,51 @@
 import Header from '@/components/Header/Header'
+import { createClient } from '@/utils/supabase/server'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default async function Login({
   searchParams,
 }: {
   searchParams: { message: string }
 }) {
+  const login = async (formData: FormData) => {
+    'use server'
+
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+    const supabase = createClient()
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      return redirect('/login?message=Invalid email or password.')
+    }
+    redirect('/')
+  }
   return (
     <div>
       <Header />
 
       <Link
         href='/'
-        className='py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4'
+        className='px-4 py-2 m-4 text-sm no-underline rounded-md text-foreground bg-btn-background hover:bg-btn-background-hover'
       >
         Home
       </Link>
 
-      <div className='w-full px-8 sm:max-w-md mx-auto mt-4'>
-        <form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4'>
+      <div className='w-full px-8 mx-auto mt-4 sm:max-w-md'>
+        <form
+          action={login}
+          className='flex flex-col justify-center flex-1 w-full gap-2 mb-4 animate-in text-foreground'
+        >
           <label className='text-md' htmlFor='email'>
             Email
           </label>
           <input
-            className='rounded-md px-4 py-2 bg-inherit border mb-6'
+            className='px-4 py-2 mb-6 border rounded-md bg-inherit'
             name='email'
             placeholder='you@example.com'
             required
@@ -32,18 +54,18 @@ export default async function Login({
             Password
           </label>
           <input
-            className='rounded-md px-4 py-2 bg-inherit border mb-6'
+            className='px-4 py-2 mb-6 border rounded-md bg-inherit'
             type='password'
             name='password'
             placeholder='••••••••'
             required
           />
-          <button className='bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2'>
+          <button className='px-4 py-2 mb-2 bg-indigo-700 rounded-md text-foreground'>
             Sign In
           </button>
 
           {searchParams?.message && (
-            <p className='mt-4 p-4 bg-foreground/10 text-foreground text-center'>
+            <p className='p-4 mt-4 text-center bg-foreground/10 text-foreground'>
               {searchParams.message}
             </p>
           )}
@@ -51,7 +73,7 @@ export default async function Login({
 
         <Link
           href='/forgot-password'
-          className='rounded-md no-underline text-indigo-400 text-sm '
+          className='text-sm text-indigo-400 no-underline rounded-md '
         >
           Forgotten Password.
         </Link>
@@ -61,7 +83,7 @@ export default async function Login({
 
         <Link
           href='/signup'
-          className='rounded-md no-underline text-foreground text-sm'
+          className='text-sm no-underline rounded-md text-foreground'
         >
           Don't have an Account? Sign Up
         </Link>
