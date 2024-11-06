e7cec7fb633fb000f4033ee5c0966a8ff73c0719
Nielven Mascariñas <mascarinas022@gmail.com>
2024-11-06 13:57:50 +0800

    update user type

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -3,13 +3,19 @@ type User = {
   email: string
   full_name: string
   created_at: string
+  job_title?: string
 }
 
 export type Member = {
   id: string
   created_at: string
   user_id: string
-  role: 'technician' | 'admin'
+  role: 'technician' | 'admin' | 'program coordinator'
   status: 'active' | 'resigned'
   users: User
+  user_metadata?: {
+    jobTitle?: string
+    full_name: string
+    avatarUrl?: string
+  }
 }
